1b292d29d001a7b72fea2e651af62215dc608fcf
gitbulk <giacatiensinh@hotmail.com>
2016-08-11 01:43:30 +0700

    done how to word

--- HowToDocX/Form1.Designer.cs
+++ HowToDocX/Form1.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSample1 = new System.Windows.Forms.Button();
+            this.btnFormatText = new System.Windows.Forms.Button();
+            this.btnReplaceText = new System.Windows.Forms.Button();
+            this.btnCreateTemplate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSample1
+            // 
+            this.btnSample1.Location = new System.Drawing.Point(19, 19);
+            this.btnSample1.Name = "btnSample1";
+            this.btnSample1.Size = new System.Drawing.Size(75, 23);
+            this.btnSample1.TabIndex = 0;
+            this.btnSample1.Text = "Sample 1";
+            this.btnSample1.UseVisualStyleBackColor = true;
+            this.btnSample1.Click += new System.EventHandler(this.btnSample1_Click);
+            // 
+            // btnFormatText
+            // 
+            this.btnFormatText.Location = new System.Drawing.Point(19, 59);
+            this.btnFormatText.Name = "btnFormatText";
+            this.btnFormatText.Size = new System.Drawing.Size(75, 23);
+            this.btnFormatText.TabIndex = 1;
+            this.btnFormatText.Text = "Format text";
+            this.btnFormatText.UseVisualStyleBackColor = true;
+            this.btnFormatText.Click += new System.EventHandler(this.btnFormatText_Click);
+            // 
+            // btnReplaceText
+            // 
+            this.btnReplaceText.Location = new System.Drawing.Point(149, 168);
+            this.btnReplaceText.Name = "btnReplaceText";
+            this.btnReplaceText.Size = new System.Drawing.Size(75, 23);
+            this.btnReplaceText.TabIndex = 2;
+            this.btnReplaceText.Text = "Replace Text";
+            this.btnReplaceText.UseVisualStyleBackColor = true;
+            this.btnReplaceText.Click += new System.EventHandler(this.btnReplaceText_Click);
+            // 
+            // btnCreateTemplate
+            // 
+            this.btnCreateTemplate.Location = new System.Drawing.Point(19, 168);
+            this.btnCreateTemplate.Name = "btnCreateTemplate";
+            this.btnCreateTemplate.Size = new System.Drawing.Size(98, 23);
+            this.btnCreateTemplate.TabIndex = 3;
+            this.btnCreateTemplate.Text = "Create Template";
+            this.btnCreateTemplate.UseVisualStyleBackColor = true;
+            this.btnCreateTemplate.Click += new System.EventHandler(this.btnCreateTemplate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnCreateTemplate);
+            this.Controls.Add(this.btnReplaceText);
+            this.Controls.Add(this.btnFormatText);
+            this.Controls.Add(this.btnSample1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSample1;
+        private System.Windows.Forms.Button btnFormatText;
+        private System.Windows.Forms.Button btnReplaceText;
+        private System.Windows.Forms.Button btnCreateTemplate;
     }
 }
 
--- HowToDocX/Form1.cs
+++ HowToDocX/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Novacode;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,106 @@ namespace HowToDocX
         public Form1()
         {
             InitializeComponent();
+            CenterToScreen();
+        }
+
+        private void btnSample1_Click(object sender, EventArgs e)
+        {
+            string fileName = @"D:\sample1.docx";
+            var doc = DocX.Create(fileName);
+            doc.InsertParagraph("this is my first paragraph");
+            doc.Save();
+            MessageBox.Show("Done");
+        }
+
+        private void btnFormatText_Click(object sender, EventArgs e)
+        {
+            string fileName = @"D:\sample2.docx";
+            var doc = DocX.Create(fileName);
+            string headlineText = "Constitution of the United States";
+            string paraOne = "We the People of the United States, in Order to form a more perfect Union, "
+                + "establish Justice, insure domestic Tranquility, provide for the common defence, "
+                + "promote the general Welfare, and secure the Blessings of Liberty to ourselves "
+                + "and our Posterity, do ordain and establish this Constitution for the United States of America.";
+
+            // A formatting object for our headline:
+            var headLineFormat = new Formatting();
+            headLineFormat.FontFamily = new System.Drawing.FontFamily("Arial Black");
+            headLineFormat.Bold = true;
+            headLineFormat.FontColor = Color.Blue;
+            headLineFormat.Size = 18D;
+            headLineFormat.Position = 12;
+
+            // A formatting object for our normal paragraph text:
+            var paraFormat = new Formatting();
+            paraFormat.FontFamily = new System.Drawing.FontFamily("Calibri");
+            paraFormat.Size = 10D;
+
+            doc.InsertParagraph(headlineText, false, headLineFormat);
+            doc.InsertParagraph(paraOne, false, paraFormat);
+            doc.Save();
+            MessageBox.Show("Done");
+        }
+
+        private void btnCreateTemplate_Click(object sender, EventArgs e)
+        {
+            // Adjust the path so suit your machine:
+            string fileName = @"D:\template_letter.docx";
+
+            // Set up our paragraph contents:
+            string headerText = "Rejection Letter";
+            string letterBodyText = DateTime.Now.ToShortDateString();
+            string paraTwo = ""
+                + "Dear %APPLICANT%" + Environment.NewLine + Environment.NewLine
+                + "I am writing to thank you for your resume. Unfortunately, your skills and "
+                + "experience do not match our needs at the present time. We will keep your "
+                + "resume in our circular file for future reference. Don't call us, we'll call you. "
+                + Environment.NewLine + Environment.NewLine
+                + "Sincerely, "
+                + Environment.NewLine + Environment.NewLine
+                + "Jim Smith, Corporate Hiring Manager";
+
+            // Title Formatting:
+            var titleFormat = new Formatting();
+            titleFormat.FontFamily = new System.Drawing.FontFamily("Arial Black");
+            titleFormat.Size = 18D;
+            titleFormat.Position = 12;
+
+            // Body Formatting
+            var paraFormat = new Formatting();
+            paraFormat.FontFamily = new System.Drawing.FontFamily("Calibri");
+            paraFormat.Size = 10D;
+            titleFormat.Position = 12;
+
+            // Create the document in memory:
+            var doc = DocX.Create(fileName);
+
+            // Insert each prargraph, with appropriate spacing and alignment:
+            Paragraph title = doc.InsertParagraph(headerText, false, titleFormat);
+            title.Alignment = Alignment.center;
+
+            doc.InsertParagraph(Environment.NewLine);
+            Paragraph letterBody = doc.InsertParagraph(letterBodyText, false, paraFormat);
+            letterBody.Alignment = Alignment.both;
+
+            doc.InsertParagraph(Environment.NewLine);
+            doc.InsertParagraph(paraTwo, false, paraFormat);
+
+            //return doc;
+            doc.Save();
+            MessageBox.Show("Done");
+        }
+
+        private void btnReplaceText_Click(object sender, EventArgs e)
+        {
+            // Adjust the path so suit your machine:
+            string fileName = @"D:\template_letter.docx";
+            DocX letter = DocX.Load(fileName);
+            string searchValue = "%APPLICANT%";
+            string newValue = "Toàn Trần";
+            letter.ReplaceText(searchValue, newValue);
+            letter.SaveAs(@"D:\result_letter.docx");
+            MessageBox.Show("Done");
         }
     }
 }
