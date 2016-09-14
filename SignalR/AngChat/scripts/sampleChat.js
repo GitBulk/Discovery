910a1a8919b00e77d976edd39010fbe99074eb41
gitbulk <giacatiensinh@hotmail.com>
2016-09-14 18:17:36 +0700

    add new projects

---
+++ SignalR/AngChat/scripts/sampleChat.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    var app = angular.module('chatApp', []);
+    app.controller('chatController', chatController);
+    chatController.$inject = ['$scope'];
+    function chatController($scope) {
+        $scope.name = 'default name';
+        $scope.message = '';
+        $scope.messages = [];
+        $scope.chatHub = null;
+
+        $scope.chatHub = $.connection.SimpleChatHub; // init hub
+        $.connection.hub.start(); // start hub
+
+        // register a client method on hub to be invoked by the server
+        $scope.chatHub.client.broadcastMessage = function (name, message) {
+            var newMessage = name + ' says: ' + message;
+
+            $scope.messages.push(newMessage);
+            $scope.$apply();
+        };
+
+        // click event
+        $scope.send = function () {
+            // sends a new message to the server
+            $scope.chatHub.server.sendMessage($scope.name, $scope.message);
+            $scope.message = '';
+        };
+    }
+})();
---
+++ SignalR/PlanSeats/SeatHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using PlanSeats.Models;
+
+namespace PlanSeats
+{
+    public class SeatHub : Hub
+    {
+        private static int UserId;
+        private static List<Seat> AllSeats = new List<Seat>();
+
+        public void CreateUser()
+        {
+            UserId++;
+            Clients.All.createUser(UserId);
+        }
+
+        public void PopulateSeatData()
+        {
+            var result = Newtonsoft.Json.JsonConvert.SerializeObject(AllSeats);
+            Clients.All.populateSeatData(result);
+        }
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
