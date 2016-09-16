19d2974a817e79cac5e344e9de239e4721c6d9b4
gitbulk <giacatiensinh@hotmail.com>
2016-09-16 14:26:20 +0700

    remove

--- SignalR/PlanSeats/SeatHub.cs
+++ SignalR/PlanSeats/SeatHub.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using PlanSeats.Models;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace PlanSeats
 {
+    [HubName("SeatHub")]
     public class SeatHub : Hub
     {
         private static int UserId;
@@ -14,19 +16,30 @@ namespace PlanSeats
 
         public void CreateUser()
         {
+            // increments the userId every time a browser opens up.
             UserId++;
             Clients.All.createUser(UserId);
         }
 
         public void PopulateSeatData()
         {
+            // Whenever a client navigates to index.html, it will be passing the seats data to the client.
             var result = Newtonsoft.Json.JsonConvert.SerializeObject(AllSeats);
             Clients.All.populateSeatData(result);
         }
 
-        public void Hello()
+        public void SelectSeat(int userId, int seatNumber)
         {
-            Clients.All.hello();
+            // Whenever a client selects a seat, it will be pushing the seat data to all of the clients.
+            var seat = new Seat()
+            {
+                SeatNumber = seatNumber,
+                UserId = userId
+            };
+            AllSeats.Add(seat);
+            var result = Newtonsoft.Json.JsonConvert.SerializeObject(seat);
+            Clients.All.selectSeat(result);
         }
+
     }
 }
