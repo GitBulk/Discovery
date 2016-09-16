145cd0d2df3324dee9ffa5d383f61e9d3487cdfb
gitbulk <giacatiensinh@hotmail.com>
2016-09-16 17:19:50 +0700

    add chat room project

---
+++ SignalR/ChatRoom/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using ChatRoom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatRoom.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Login model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Title = "Home Page";
+                ViewBag.Username = model.UserName;
+                return View("Chat");
+            }
+            return View("Index");
+        }
+
+        public ActionResult Chat(Login model)
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SignalR/ChatRoom/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChatRoom.MvcApplication" Language="C#" %>
