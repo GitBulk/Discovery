6fe337fcf4ce7046148ac8111c1f34b10b11ed84
gitbulk <giacatiensinh@hotmail.com>
2016-09-23 17:29:59 +0700

    staged

---
+++ Angular/CustomManagement/Scripts/app/customersApp/services/dataService.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('customersApp').factory('dataService', dataService);
+    dataService.$inject = ['config', 'customersService', 'customersBreezeService']
+    function dataService(config, customersService, customersBreezeService) {
+        return (config.useBreeze) ? customersBreezeService : customersService;
+    };
+
+})();
--- Angular/CustomManagement/app/customersApp/app.js
+++
@@ -1,3 +0,0 @@
-﻿(function () {
-    var app = angular.module('customersApp', []);
-})();
