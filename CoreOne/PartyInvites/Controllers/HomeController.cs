012ca19c2524df5ae5acd554fce3259ff2327f5e
gitbulk <giacatiensinh@hotmail.com>
2016-10-31 16:36:50 +0700

    add new

---
+++ CoreOne/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse model)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(model);
+                return View("Thanks", model);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Thanks()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
