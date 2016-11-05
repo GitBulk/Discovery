9bb820d0d76de20e58f3bc073c66a716d02d241e
lasthulk <danchithancongab@gmail.com>
2016-11-05 15:59:45 +0700

    add new project

---
+++ MVC5/BuildLinks/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace BuildLinks.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
