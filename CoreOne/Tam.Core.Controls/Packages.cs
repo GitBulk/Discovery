b9f08289e5cd337885e7e848fc578aa8c4edf5e0
gitbulk <giacatiensinh@hotmail.com>
2016-11-09 18:19:18 +0700

    add new project

---
+++ CoreOne/Tam.Core.Controls/Packages.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tam.Core.Controls
+{
+    public static class Packages
+    {
+        private static Dictionary<string, HtmlString> Container = new Dictionary<string, HtmlString>();
+
+        public static HtmlString Render(string packageName)
+        {
+            if (Container.ContainsKey(packageName))
+            {
+                return Container[packageName];
+            }
+            return HtmlString.Empty;
+        }
+
+        public static HtmlString AddScript(this IHtmlHelper helper, string packageName, params string[] filePaths)
+        {
+            string format = string.Format("<script src=\"{0}\"></script>");
+            return AddFile(packageName, format, filePaths);
+        }
+
+        public static HtmlString AddStyle(this IHtmlHelper helper, string packageName, params string[] filePaths)
+        {
+            string format = string.Format("<link rel=\"stylesheet\" href=\"{0}\" />");
+            return AddFile(packageName, format, filePaths);
+        }
+
+        private static HtmlString AddFile(string packageName, string format, string[] filePaths)
+        {
+            var builder = new StringBuilder();
+            if (!string.IsNullOrWhiteSpace(format) && filePaths != null && filePaths.Any())
+            {
+                foreach (var item in filePaths)
+                {
+                    if (string.IsNullOrWhiteSpace(item) || item.EndsWith(".css", StringComparison.OrdinalIgnoreCase) == false ||
+                        item.EndsWith(".js", StringComparison.OrdinalIgnoreCase) == false)
+                    {
+                        throw new Exception("File is invalid");
+                    }
+                    builder.AppendLine(string.Format(format, item));
+                }
+                var finalResult = new HtmlString(builder.ToString());
+                Container.Add(packageName, finalResult);
+                return finalResult;
+            }
+            return HtmlString.Empty;
+        }
+    }
+}
--- CoreOne/Tam.Core.Controls/Properties/AssemblyInfo.cs
+++ CoreOne/Tam.Core.Controls/Properties/AssemblyInfo.cs
@@ -5,14 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CustomManagement")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CustomManagement")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Tam.Core.Controls")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,16 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a1d19631-9a81-4954-b3e7-4cc4de14076d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("bde4ea1f-3ad3-4460-9e03-e5ad2471a961")]
