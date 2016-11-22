b0e56edc7ad39d37f471cd9982eb64976d676e1b
gitbulk <giacatiensinh@hotmail.com>
2016-11-22 18:05:17 +0700

    update

---
+++ CoreOne/PatientRecords/Models/Human.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientRecords.Models
+{
+    public class Human
+    {
+        public int ID { get; set; }
+        public string SocialSecurityNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- CoreOne/PatientRecords/wwwroot/_references.js
+++ CoreOne/PatientRecords/wwwroot/_references.js
@@ -1,5 +1,4 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="../gruntfile.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
