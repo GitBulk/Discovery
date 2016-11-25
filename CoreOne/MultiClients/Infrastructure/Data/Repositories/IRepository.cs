75bb8b1de3a00c8a640cce788f92d842fadbdf7c
gitbulk <giacatiensinh@hotmail.com>
2016-11-25 11:59:25 +0700

    add new Project

---
+++ CoreOne/MultiClients/Infrastructure/Data/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MultiClients.Infrastructure.Data.Repositories
+{
+    public interface IRepository<TEntity>
+    {
+        #region READ
+        TEntity Get(Expression<Func<TEntity, bool>> predicate);
+        TEntity Get(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties);
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> GetAll(params Expression<Func<TEntity, object>>[] includeProperties);
+
+        #endregion
+    }
+}
