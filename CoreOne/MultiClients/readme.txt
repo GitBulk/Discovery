53beabf08d2740b63d49be3b4c24c1f32d6e9289
gitbulk <giacatiensinh@hotmail.com>
2016-12-03 16:35:11 +0700

    update

--- CoreOne/MultiClients/readme.txt
+++ CoreOne/MultiClients/readme.txt
@@ -1,2 +1,6 @@
-﻿Run the following command to create a model from the existing database (Package manager console)
+﻿https://docs.microsoft.com/en-us/ef/core/get-started/aspnetcore/existing-db
+
+
+Run the following command to create a model from the existing database (Package manager console)
 Scaffold-DbContext "Server=localhost;Database=Chinook;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Entities
+
---
+++ CoreOne/PartyInvites/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PartyInvites.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // private static Logger Logger = LogManager.GetCurrentClassLogger();
+        private ILogger<ValuesController> logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            this.logger = logger;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            this.logger.LogCritical("nlog is working from a controller");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
