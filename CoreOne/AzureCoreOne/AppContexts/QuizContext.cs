17edc0d1370aaf3fe3f00b57f4a3742ec16fed72
gitbulk <giacatiensinh@hotmail.com>
2016-12-07 11:26:54 +0700

    add new classes

---
+++ CoreOne/AzureCoreOne/AppContexts/QuizContext.cs
@@ -0,0 +1,25 @@
+﻿using AzureCoreOne.Models.Quizs;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureCoreOne.AppContexts
+{
+    public class QuizContext : DbContext
+    {
+        public DbSet<Quiz> Quizzes { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        // Best practice is to allow the options to be
+        // passed into a constructor so that we remove any
+        // assumptions about where the data is stored: inmemory,
+        // SQL Server, and so on.
+        public QuizContext(DbContextOptions options) :
+        base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder
+        modelBuilder)
+        {
+            modelBuilder.Entity<Quiz>().HasMany<Question>
+            ().WithOne(q => q.Quiz);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ CoreOne/AzureCoreOne/ViewModels/FinishViewModel.cs
@@ -0,0 +1,12 @@
+﻿using AzureCoreOne.Models.Quizs;
+using System.Collections.Generic;
+
+namespace AzureCoreOne.ViewModels
+{
+    public class FinishViewModel
+    {
+        public Quiz Quiz { get; set; }
+        public Dictionary<int, string> Answers { get; set; }
+        public int CorrectAnswers { get; set; }
+    }
+}
