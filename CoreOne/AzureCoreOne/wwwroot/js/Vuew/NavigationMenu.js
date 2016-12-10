053ef358fb660bd4455665f8a57a370bef768068
gitbulk <giacatiensinh@hotmail.com>
2016-12-10 10:24:48 +0700

    update navigation page

--- CoreOne/AzureCoreOne/wwwroot/js/Vuew/NavigationMenu.js
+++ CoreOne/AzureCoreOne/wwwroot/js/Vuew/NavigationMenu.js
@@ -1,30 +1,11 @@
-﻿// define a custom filter called currency
-Vue.filter('currency', function (value) {
-    return '$' + value.toFixed(2);
-});
-
-var orderFormApp = new Vue({
-    el: '#orderFormApp',
+﻿var navigationApp = new Vue({
+    el: '#main',
     data: {
-        services: [
-            { name: "Web Developement", price: 300, active: true },
-            { name: "Desgin", price: 400, active: false },
-            { name: "Integration", price: 250, active: false },
-            { name: "Training", price: 220, active: false }
-        ]
+        active: 'home'
     },
     methods: {
-        toggleActive: function (item) {
-            item.active = !item.active;
-        },
-        total: function () {
-            var total = 0;
-            this.services.forEach(function (item) {
-                if (item.active) {
-                    total += item.price;
-                }
-            });
-            return total;
+        makeActive: function (item) {
+            this.active = item;
         }
     }
 });
