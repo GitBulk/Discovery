133f75092db16226e83741a5dec97b9eb313bb69
gitbulk <giacatiensinh@hotmail.com>
2016-12-21 17:14:13 +0700

    add helper for HttpContext in .Net Core

---
+++ CoreOne/Tam.Core/Utilities/HttpContextHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tam.Core.Utilities
+{
+    public static class HttpContextHelper
+    {
+        public static IHttpConnectionFeature GetConnectionFeature(this HttpContext context)
+        {
+            return context.Features.Get<IHttpConnectionFeature>();
+        }
+    }
+}
