2fd7965cf096552f95254f7aca6dac9a4b197a89
gitbulk <giacatiensinh@hotmail.com>
2016-12-22 10:06:33 +0700

    add helper method

--- CoreOne/Tam.Core/Utilities/HttpContextHelper.cs
+++ CoreOne/Tam.Core/Utilities/HttpContextHelper.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Tam.Core.Utilities
 {
@@ -12,6 +13,16 @@ namespace Tam.Core.Utilities
         public static IHttpConnectionFeature GetHttpConnectionFeature(this HttpContext context)
         {
             return context.Features.Get<IHttpConnectionFeature>();
+        }
+
+        public static void AddHttpResponseHeader(this HttpContext context, string key, string value)
+        {
+            Guard.ThrowIfNullOrWhiteSpace(key);
+            context.Response.Headers.Add(key, value);
+        }
+        public static void AddHttpResponseHeader(this ResultExecutingContext context, string key, string value)
+        {
+            context.HttpContext.AddHttpResponseHeader(key, value);
         }
     }
 }
