50c3a04f5af439c79d47d9679c07288ded8f546d
gitbulk <giacatiensinh@hotmail.com>
2016-12-23 18:02:45 +0700

    update

---
+++ CoreOne/Tam.Core/Utilities/ApplicationBuilderExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tam.Core.Utilities
+{
+    public static class ApplicationBuilderExtension
+    {
+        public static IApplicationBuilder Default(this IApplicationBuilder app, IList<CultureInfo> cultures, RequestCulture
+            defaultCulture)
+        {
+            var supportedCultures = new List<CultureInfo>
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("vi-VN")
+            };
+            if (cultures != null && cultures.Count > 0)
+            {
+                foreach (var item in cultures)
+                {
+                    if (supportedCultures.Find(c => c.Name == item.Name) == null)
+                    {
+                        supportedCultures.Add(item);
+                    }
+                }
+            }
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = defaultCulture,
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
+            return app;
+        }
+    }
+}
