0e30d3659ef3a65856b82029c23fe404e91b9d83
gitbulk <giacatiensinh@hotmail.com>
2016-12-30 17:54:42 +0700

    good

--- CoreOne/QueueReceiver/QueueReceiver/Program.cs
+++ CoreOne/QueueReceiver/QueueReceiver/Program.cs
@@ -28,53 +28,29 @@ namespace QueueReceiver
             Console.WriteLine("I am listening");
             var factory = CreateConnectionFactory(hostName);
             using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
             {
-                using (var channel = connection.CreateModel())
+                channel.QueueDeclare(queueName,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
                 {
-                    channel.QueueDeclare(queueName,
-                                         durable: false,
-                                         exclusive: false,
-                                         autoDelete: false,
-                                         arguments: null);
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello",
+                                     noAck: true,
+                                     consumer: consumer);
 
-                    var consumer = new EventingBasicConsumer(channel);
-                    //consumer.Received += (model, ea) =>
-                    //{
-                    //    var body = ea.Body;
-                    //    var message = Encoding.UTF8.GetString(body);
-                    //    Console.WriteLine(" [x] Received {0}", message);
-                    //};
-                    consumer.Received += Consumer_Received;
-
-                    channel.BasicConsume(queueName,
-                                         noAck: true,
-                                         consumer: consumer);
-
-
-                    Console.WriteLine(" Press [enter] to exit.");
-                    Console.ReadLine();
-                }
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
             }
-
-
-            //var queueManager = new QueueManager(hostName);
-            ////queueManager.Receive(queueName);
-            //queueManager.Receive(queueName, Consumer_Received);
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
         }
-
-
-        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
-        {
-            var body = e.Body;
-            var message = Encoding.UTF8.GetString(body);
-
-            // do some thing with message
-            Console.WriteLine(message);
-            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff"));
-        }
-
 
     }
 
--- CoreOne/QueueSender/Program.cs
+++ CoreOne/QueueSender/Program.cs
@@ -16,7 +16,7 @@ namespace QueueSender
             var factory = new ConnectionFactory()
             {
                 //HostName = hostName
-                Uri = uri
+                Uri = new Uri(hostName).AbsoluteUri
             };
             return factory;
         }
@@ -24,29 +24,25 @@ namespace QueueSender
         static void Main(string[] args)
         {
             string hostName = ConfigurationManager.AppSettings["QueueConnection"];
-            //var factory = CreateConnectionFactory(hostName);
-            //using (var connection = factory.CreateConnection())
-            //using (var channel = connection.CreateModel())
-            //{
-            //    channel.QueueDeclare(queue: "hello",
-            //                         durable: false,
-            //                         exclusive: false,
-            //                         autoDelete: false,
-            //                         arguments: null);
+            var factory = CreateConnectionFactory(hostName);
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
 
-            //    string message = "Hello World!";
-            //    var body = Encoding.UTF8.GetBytes(message);
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
 
-            //    channel.BasicPublish(exchange: "",
-            //                         routingKey: "hello",
-            //                         basicProperties: null,
-            //                         body: body);
-            //    Console.WriteLine(" [x] Sent {0}", message);
-            //}
-            string queueName = "hello";
-            var queueManager = new QueueManager(hostName);
-            Console.WriteLine("Start at: " + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff"));
-            queueManager.Send(queueName, "I am batman");
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
