3ff7b18114315d27d4be82b951f72d93e47df4d9
gitbulk <giacatiensinh@hotmail.com>
2017-01-05 16:05:26 +0700

    add a new base class for Queue event

---
+++ CoreOne/Tam.Core/RabbitMQ/BaseEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tam.Core.RabbitMQ
+{
+    public abstract class BaseEntry
+    {
+        public string Id { get; set; }
+        public string What { get; set; }
+        public DateTime When { get; set; }
+        public string Who { get; set; }
+    }
+}
