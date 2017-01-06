13d622c7be1ea00fccf63a57b218142b0bb8f43c
gitbulk <giacatiensinh@hotmail.com>
2017-01-06 17:44:35 +0700

    update test

--- CoreOne/TestCoreOne/UnitTest1.cs
+++ CoreOne/TestCoreOne/UnitTest1.cs
@@ -13,56 +13,152 @@ namespace TestCoreOne
         [TestMethod]
         public void TestEmail()
         {
+            string email = "kuti1410@yahoo.com.vn";
             //string email = "toantran222@aperia.vn";
-            string email = "toan.tran@d2cgroup.com";
-            //var validator = new Codicode.EmailValidator();
-            //validator.Mail_From = "admin@monoprog.com";
-            //string msg = validator.Check_MailBox_Error(email);
+            //string email = "toan.tran@d2cgroup.com";
+            //string email = "ash_foster@hotmail.com";
+            var validator = new Codicode.EmailValidator();
+            validator.Mail_From = "admin@monoprog.com";
+            string msg = validator.Check_MailBox_Error(email);
             //bool result = validator.Check_MailBox(email);
             //result = validator.Check_SMTP(email);
             //validator.Dispose();
 
-            var emails = new List<string>()
-            {
-                "toan.tran@d2cgroup.com",
-                "toantran@aperia.vn"
-            };
+            //string filePath = @"D:\emails.txt";☺
 
-            string msg = btnCheck_Click(email);
+            //int counter = 0;
+            //string line;
+
+            //// Read the file and display it line by line.
+            //var fileReader = new StreamReader(filePath);
+            //string msg = string.Empty;
+            //var builder = new StringBuilder();
+            //while ((line = fileReader.ReadLine()) != null)╟
+            //{
+            //    //Console.WriteLine(line);
+            //    counter++;
+            //    if (string.IsNullOrWhiteSpace(line) == false)
+            //    {
+            //        msg = btnCheck_Click(line.Trim());
+            //        builder.AppendLine(msg + " " + line);
+            //    }
+            //}
+
+            //fileReader.Close();
+
+            //File.WriteAllText("D:\\result.txt", builder.ToString(), Encoding.UTF8);
+
+            //var emails = new List<string>()
+            //{
+            //    "toan.tran@d2cgroup.com",
+            //    "toantran@aperia.vn"
+            //};
+
+
+
+            //string msg = CheckMail(email, "smtp.mail.yahoo.com", 25);
             Assert.IsTrue(msg == "");
         }
 
-        protected string btnCheck_Click(string email)
+        protected string CheckMail(string email, string smtp = "gmail-smtp-in.l.google.com", int port = 25)
         {
-            TcpClient tClient = new TcpClient("gmail-smtp-in.l.google.com", 25);
+
+            //TcpClient tClient = new TcpClient(smtp, port);
+            //string CRLF = "\r\n";
+            //byte[] dataBuffer;
+            //string ResponseString;
+            //NetworkStream netStream = tClient.GetStream();
+            //StreamReader reader = new StreamReader(netStream);
+            //ResponseString = reader.ReadLine();
+            ///* Perform HELO to SMTP Server and get Response */
+            //dataBuffer = BytesFromString("HELO KirtanHere" + CRLF);
+            //netStream.Write(dataBuffer, 0, dataBuffer.Length);
+            //ResponseString = reader.ReadLine();
+            //dataBuffer = BytesFromString("MAIL FROM:<test.email.greetratereal@gmail.com>" + CRLF);
+            //netStream.Write(dataBuffer, 0, dataBuffer.Length);
+            //ResponseString = reader.ReadLine();
+            ///* Read Response of the RCPT TO Message to know from google if it exist or not */
+            //dataBuffer = BytesFromString("RCPT TO:<" + email + ">" + CRLF);
+            //netStream.Write(dataBuffer, 0, dataBuffer.Length);
+            //ResponseString = reader.ReadLine();
+            //string msg = "1";
+            //if (GetResponseCode(ResponseString) == 550)
+            //{
+            //    //msg += ("Mai Address Does not Exist !<br/><br/>");
+            //    //msg += ("<B><font color='red'>Original Error from Smtp Server :</font></b>" + ResponseString);
+            //    msg = "0";
+            //}
+            ///* QUITE CONNECTION */
+            //dataBuffer = BytesFromString("QUITE" + CRLF);
+            //netStream.Write(dataBuffer, 0, dataBuffer.Length);
+            //tClient.Close();
+            //return msg;
+
+            // change cursor into wait cursor
+            // create server SMTP with port 25
+            TcpClient SmtpServ = new TcpClient(smtp, 25);
+            string Data;
+            byte[] szData;
             string CRLF = "\r\n";
-            byte[] dataBuffer;
-            string ResponseString;
-            NetworkStream netStream = tClient.GetStream();
-            StreamReader reader = new StreamReader(netStream);
-            ResponseString = reader.ReadLine();
-            /* Perform HELO to SMTP Server and get Response */
-            dataBuffer = BytesFromString("HELO KirtanHere" + CRLF);
-            netStream.Write(dataBuffer, 0, dataBuffer.Length);
-            ResponseString = reader.ReadLine();
-            dataBuffer = BytesFromString("MAIL FROM:<admin@monoprog.com>" + CRLF);
-            netStream.Write(dataBuffer, 0, dataBuffer.Length);
-            ResponseString = reader.ReadLine();
-            /* Read Response of the RCPT TO Message to know from google if it exist or not */
-            dataBuffer = BytesFromString("RCPT TO:<" + email + ">" + CRLF);
-            netStream.Write(dataBuffer, 0, dataBuffer.Length);
-            ResponseString = reader.ReadLine();
-            string msg = "";
-            if (GetResponseCode(ResponseString) == 550)
+            List<string> LogList = new List<string>();
+
+            try
             {
-                msg += ("Mai Address Does not Exist !<br/><br/>");
-                msg += ("<B><font color='red'>Original Error from Smtp Server :</font></b>" + ResponseString);
+                // initialization
+                NetworkStream NetStrm = SmtpServ.GetStream();
+                StreamReader RdStrm = new StreamReader(SmtpServ.GetStream());
+                LogList.Add(RdStrm.ReadLine());
+
+                // say hello to server and send response into log report
+                Data = "HELLO server " + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                LogList.Add(RdStrm.ReadLine());
+                // send sender data
+                Data = "MAIL FROM: " + "<test.email.greetratereal@gmail.com>" + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                LogList.Add(RdStrm.ReadLine());
+
+                // send receiver data
+                Data = "RCPT TO: " + "<" + email + ">" + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                LogList.Add(RdStrm.ReadLine());
+
+                // send DATA
+                Data = "DATA " + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                LogList.Add(RdStrm.ReadLine());
+
+                // send content data
+                Data = "SUBJECT: Hello";
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                LogList.Add(RdStrm.ReadLine());
+
+                // quit from server SMTP
+                Data = "QUIT " + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                LogList.Add(RdStrm.ReadLine());
+
+                // close connection
+                NetStrm.Close();
+                RdStrm.Close();
+                LogList.Add("Close connection");
+                LogList.Add("Send mail successly..");
+
+                // back to normal cursor
+                //Cursor.Current = cr;
+                return LogList.ToString();
             }
-            /* QUITE CONNECTION */
-            dataBuffer = BytesFromString("QUITE" + CRLF);
-            netStream.Write(dataBuffer, 0, dataBuffer.Length);
-            tClient.Close();
-            return msg;
+            catch (InvalidOperationException err)
+            {
+                LogList.Add("Error: " + err.ToString());
+                return err.ToString();
+            }
         }
 
         private byte[] BytesFromString(string str)
